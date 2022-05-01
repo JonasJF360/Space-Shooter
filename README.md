3a6da705fc9c70c05c2e6d3543fd2c29bbe37d81
JonasJF360 <jonasjf360@gmail.com>
2022-05-01 19:27:06 -0400

    Atualização e melhorias. Adição de um botão para iniciar o jogo

--- README.md
+++ README.md
@@ -1,11 +1,39 @@
-# Space-Shooter
-- Eu não sou o criador original desse jogo, mas como gostei muito do projeto, resolvi fazer um fork para modificar e experimentar minha "própria" versão.
-- Foi e ainda esta sendo muito bom alterar esse código, ajudou bastante no meu aprendizado e pude colocar em prática minhas habilidades.
-- Além de ser um bom desafio é muito legal ver o código de outra pessoa e a partir daí tentar entender fazer nossas próprias modificações. <br><br>
-## [Clique aqui]( https://jonasjf360.github.io/Space-Shooter/) para jogar.
+# Space Shooter II
+
+> Baseado na versão de Victor Barbosa, criei esse jogo a fim de fixar meus conhecimentos em html, css e javascript.
+
+## Olá, esse é jogo nostalgia Space Shooter II
+
+> O objetivo principal do jogo é bem simples, você só tem que conseguir destruir o maior número de vaves inimigas possível.
+> Caso queira jogar clique no link abaixo e boa sorte.
+
+### [Clique aqui]( https://jonasjf360.github.io/Space-Shooter/) para jogar.
+
+----------------------------------------------------
+**Minha versão conta com algumas mudanças visuais:**
+- Atualização do visual das naves;
+- Adição de um menu de ajuda;
+- Adição de um botão de start;
+- Atualização da interface para maior responsividade em dispositivos diferentes;
+- Adição de botões para tornar possível jogar em telas sensíveis ao toque;
+- Opção de mudar o idioma (Português, Inglês).
+
+**Mudanças não visuais (código):**
+- Uma nova função que aumenta a dificuldade do jogo gradativamente a cada 8 naves inimigas destruídas.
+- Alteração do código para escopos mais atuais;
+- Mudança na forma de inicialização do jogo que antes recarregava a página para iniciar.
+
+**Mudanças em construção ou incompletas:**
+- Quebrar o código em pequenas funções para facilitar a leitura e manutenção. (Em andamento);
+- Identificação de bugs e melhoria no desempenho. (Em andamento).
+
+<br>
+
 ![image](./assets/images/screenshot.png)
 
--------------------------
-## O código original pertence a Victor Barbosa
+<br>
+
+--------------------------------------------------------------
+## Criei esse jogo baseado na versão criada por Victor Barbosa.
 
 - [Clique aqui](https://github.com/V1ctorBarbosa/Space-Shooter) caso queira acessar.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,84 +1,22 @@
 (( /* App */ ) => {
+    let firstGameStart = true;
+    const gameOver = document.querySelector('#gameOver');
+    const startGame = document.querySelector('.startGame');
+
+    const mainHelp = document.querySelector('.main-help')
+    const level = document.querySelector('.level span');
+    const pointsBox = document.querySelector('.points-box');
 
     const board = document.querySelector('#board');
     let widthBord = board.offsetWidth;
     let heightBord = board.offsetHeight;
 
     const jet = document.querySelector('#jet');
-    const pointsBox = document.querySelector('.points-box');
     const rocks = document.getElementsByClassName('rocks');
-    const gameOver = document.querySelector('#gameOver');
 
-    const mainHelp = document.querySelector('.main-help')
     const shot = document.querySelector('#shot');
     const explosion = document.querySelector('#explosion');
 
-    const level = document.querySelector('.level span');
-
-
-    window.addEventListener('keydown', (e) => {
-        let tecla = e.code;
-        let left = parseInt(window.getComputedStyle(jet).getPropertyValue('left'));
-        let bullet = document.createElement('div');
-
-        if (tecla === 'ArrowLeft' && left > 0) toLeft();
-        if (tecla === 'ArrowRight' && left <= widthBord - 40) toRigth();
-        if (tecla === 'ArrowUp' || tecla === 'Space') activeGun();
-
-        function toLeft() {
-            jet.style.left = left - 10 + 'px';
-        }
-
-        function toRigth() {
-            jet.style.left = left + 10 + 'px';
-        }
-
-        function activeGun() {
-            bullet.classList.add('bullets');
-            board.appendChild(bullet);
-            shot.play();
-            shot.currentIime = 0;
-
-            const movebullet = setInterval(() => {
-                for (let rock of rocks) {
-                    if (rock !== 'undefined') {
-                        let rockbound = rock.getBoundingClientRect();
-                        let bulletbound = bullet.getBoundingClientRect();
-                        if (
-                            bulletbound.left >= rockbound.left &&
-                            bulletbound.right <= rockbound.right &&
-                            bulletbound.top <= rockbound.top &&
-                            bulletbound.bottom <= rockbound.bottom
-                        ) {
-                            rock.parentElement.removeChild(rock);
-                            pointsBox.textContent =
-                                parseInt(pointsBox.textContent) + 1;
-                            explosion.play();
-                            explosion.currentIime = 0;
-                            kill--;
-                            if (dificult === minDificult) {
-                                level.innerText = 'Max'
-                            } else if (kill === 0) {
-                                if (currentIntervalId) clearInterval(currentIntervalId);
-                                dificultGame();
-                                atualLevel++;
-                                level.innerText = atualLevel
-
-                                kill = maxlevel;
-                            }
-                        }
-                    }
-                }
-                let bulletbottom = parseInt(
-                    window.getComputedStyle(bullet).getPropertyValue('bottom')
-                );
-                if (bulletbottom >= 500) clearInterval(movebullet);
-
-                bullet.style.left = left + 'px';
-                bullet.style.bottom = bulletbottom + 3 + 'px';
-            });
-        }
-    });
 
     const maxlevel = 8;
     const minDificult = 800;
@@ -87,46 +25,117 @@
     let kill = maxlevel;
     let atualLevel = 1;
 
-
-    window.addEventListener('load', () => {
-        dificultGame();
-    });
-
     function dificultGame() {
         dificult <= minDificult ? dificult = minDificult : dificult -= 500;
 
-        currentIntervalId = setInterval(() => {
-            if (widthBord !== board.offsetWidth) widthBord = board.offsetWidth;
-            if (heightBord !== board.offsetHeight) heightBord = board.offsetHeight;
-
-            let rock = document.createElement('div');
-            rock.classList.add('rocks');
-            rock.style.left = Math.floor(Math.random() * (widthBord - 50)) + 'px';
-
-            board.appendChild(rock);
-        }, dificult);
+        currentIntervalId = setInterval(createRocks, dificult);
     }
 
+    function updateDificult() {
+        if (dificult !== minDificult) kill--;
+        if (dificult === minDificult) level.innerText = 'Max'
+        if (kill === 0) {
+            if (currentIntervalId) clearInterval(currentIntervalId);
+            dificultGame();
+            atualLevel++;
+            level.innerText = atualLevel
 
-    const moverocks = setInterval(() => {
-
-        if (rocks != undefined) {
-            for (let i = 0; i < rocks.length; i++) {
-                let rock = rocks[i];
-                let rocktop = parseInt(
-                    window.getComputedStyle(rock).getPropertyValue('top')
-                );
-                if (rocktop >= heightBord - 25) {
-                    clearInterval(moverocks);
-                    clearInterval(currentIntervalId);
-                    gameOver.style.display = 'block'
-                    return
-                }
-
-                rock.style.top = rocktop + 25 + 'px';
-            }
+            kill = maxlevel;
         }
-    }, 450);
+    }
+
+    function createRocks() {
+        if (widthBord !== board.offsetWidth) widthBord = board.offsetWidth;
+        if (heightBord !== board.offsetHeight) heightBord = board.offsetHeight;
+
+        let rock = document.createElement('div');
+        rock.classList.add('rocks');
+        rock.style.left = Math.floor(Math.random() * (widthBord - 50)) + 'px';
+
+        board.appendChild(rock);
+    }
+
+    function startTheGame() {
+        window.addEventListener('keydown', keyboardCapture);
+
+        function keyboardCapture(e) {
+            let tecla = e.code;
+
+            let left = parseInt(window.getComputedStyle(jet).getPropertyValue('left'));
+            let bullet = document.createElement('div');
+
+            if (tecla === 'ArrowLeft' && left > 0) toLeft();
+            if (tecla === 'ArrowRight' && left <= widthBord - 40) toRigth();
+            if (tecla === 'ArrowUp' || tecla === 'Space') activeGun();
+
+            function toLeft() {
+                jet.style.left = left - 10 + 'px';
+            }
+
+            function toRigth() {
+                jet.style.left = left + 10 + 'px';
+            }
+
+            function activeGun() {
+                bullet.classList.add('bullets');
+                board.appendChild(bullet);
+                shot.play();
+                shot.currentIime = 0;
+
+                const movebullet = setInterval(() => {
+
+                    for (let rock of rocks) {
+                        if (rock !== 'undefined') {
+                            let rockbound = rock.getBoundingClientRect();
+                            let bulletbound = bullet.getBoundingClientRect();
+                            if (
+                                bulletbound.left >= rockbound.left &&
+                                bulletbound.right <= rockbound.right &&
+                                bulletbound.top <= rockbound.top &&
+                                bulletbound.bottom <= rockbound.bottom
+                            ) {
+                                rock.parentElement.removeChild(rock);
+                                pointsBox.textContent =
+                                    parseInt(pointsBox.textContent) + 1;
+                                explosion.play();
+                                explosion.currentIime = 0;
+
+                                updateDificult();
+                            }
+                        }
+                    }
+                    let bulletbottom = parseInt(
+                        window.getComputedStyle(bullet).getPropertyValue('bottom')
+                    );
+                    if (bulletbottom >= 500) clearInterval(movebullet);
+
+                    bullet.style.left = left + 'px';
+                    bullet.style.bottom = bulletbottom + 3 + 'px';
+                });
+            }
+        };
+
+
+        const moverocks = setInterval(() => {
+
+            if (rocks != undefined) {
+                for (let rock of rocks) {
+                    let rocktop = parseInt(
+                        window.getComputedStyle(rock).getPropertyValue('top')
+                    );
+                    if (rocktop >= heightBord - 25) {
+                        clearInterval(moverocks);
+                        clearInterval(currentIntervalId);
+                        window.removeEventListener('keydown', keyboardCapture);
+                        gameOver.style.display = 'block'
+                        return;
+                    }
+
+                    rock.style.top = rocktop + 25 + 'px';
+                }
+            }
+        }, 450);
+    }
 
 
     document.onclick = restartGame;
@@ -134,8 +143,21 @@
     function restartGame(e) {
         if (e.target.id === 'help') mainHelp.style.display = 'flex';
         if (e.target.id === 'close') mainHelp.style.display = 'none';
-        if (e.target.id === 'restart') return window.location.reload();
-
+        if (e.target.id === 'start' || e.target.id === 'restart') {
+            if (firstGameStart) {
+                firstGameStart = false;
+                startGame.style.display = 'none'
+            } else {
+                dificult = 5000;
+                atualLevel = 1;
+                pointsBox.textContent = 0;
+                [...document.getElementsByClassName("bullets")].map(n => n && n.remove());
+                [...document.getElementsByClassName("rocks")].map(n => n && n.remove());
+                gameOver.style.display = 'none'
+            }
+            dificultGame();
+            startTheGame();
+        }
         if (e.target.id === 'active-gun') {
             window.dispatchEvent(new KeyboardEvent('keydown', {
                 code: 'Space'
@@ -151,17 +173,6 @@
                 code: 'ArrowRight'
             }));
         }
-        if (e.target.id === 'to-left') {
-            window.dispatchEvent(new KeyboardEvent('keydown', {
-                code: 'ArrowLeft'
-            }));
-        }
-        if (e.target.id === 'to-rigth') {
-            window.dispatchEvent(new KeyboardEvent('keydown', {
-                code: 'ArrowRight'
-            }));
-        }
-
     }
 
 })();
